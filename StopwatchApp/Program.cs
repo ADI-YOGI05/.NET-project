a55bed05e36d0f09785c40bf4a719a20adab3e26
ADI-YOGI05 <113439383+ADI-YOGI05@users.noreply.github.com>
2023-12-11 23:11:20 +0530

    Add files via upload

---
+++ StopwatchApp/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace Stopwatch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IState stopwatch = new Context(new InitialScreenState());
+
+            // Press 2nd button => nothing happens
+            PrintCurrentState(stopwatch);
+
+            stopwatch = stopwatch.PressSecondButton();
+            PrintCurrentState(stopwatch);
+
+            // Start, Lap, Lap, Stop, Restart, Lap, Stop, Reset
+            stopwatch = stopwatch.PressFirstButton(); // Started
+            Console.WriteLine($"StopWatch {stopwatch.TextOnFirstButton}");
+            PrintCurrentState(stopwatch);
+
+            stopwatch = stopwatch.PressSecondButton(); // Lap
+            Console.WriteLine($"StopWatch {stopwatch.TextOnFirstButton}");
+            PrintCurrentState(stopwatch);
+
+            stopwatch = stopwatch.PressSecondButton(); // Lap
+            Console.WriteLine($"StopWatch {stopwatch.TextOnFirstButton}");
+            PrintCurrentState(stopwatch);
+
+            stopwatch = stopwatch.PressFirstButton(); // Stop
+            Console.WriteLine($"StopWatch {stopwatch.TextOnFirstButton}");
+            PrintCurrentState(stopwatch);
+
+            stopwatch = stopwatch.PressFirstButton(); // Restart
+            Console.WriteLine($"StopWatch {stopwatch.TextOnFirstButton}");
+            PrintCurrentState(stopwatch);
+
+            stopwatch = stopwatch.PressSecondButton(); // Lap
+            Console.WriteLine($"StopWatch {stopwatch.TextOnFirstButton}");
+            PrintCurrentState(stopwatch);
+
+            stopwatch = stopwatch.PressFirstButton(); // Stop
+            Console.WriteLine($"StopWatch {stopwatch.TextOnFirstButton}");
+            PrintCurrentState(stopwatch);
+
+            stopwatch = stopwatch.PressSecondButton(); // Reset
+            Console.WriteLine($"StopWatch {stopwatch.TextOnFirstButton}");
+            PrintCurrentState(stopwatch);
+        }
+
+        static void PrintCurrentState(IState stopwatch)
+        {
+            Console.WriteLine($"Current State: {stopwatch.GetType().Name}, Button1 [{stopwatch.TextOnFirstButton}], Button2 [{stopwatch.TextOnSecondButton}]");
+        }
+    }
+}
---
+++ StopwatchApp/Stopwatch.cs
@@ -0,0 +1,99 @@
+﻿internal interface IState
+{
+    string TextOnFirstButton { get; }
+    string TextOnSecondButton { get; }
+
+    IState PressFirstButton();
+    IState PressSecondButton();
+}
+
+internal class Context : IState
+{
+    private IState currentState;
+
+    public Context(IState initialState)
+    {
+        currentState = initialState;
+    }
+
+    public string TextOnFirstButton => currentState.TextOnFirstButton;
+    public string TextOnSecondButton => currentState.TextOnSecondButton;
+
+    public IState PressFirstButton()
+    {
+        currentState = currentState.PressFirstButton();
+        return currentState;
+    }
+
+    public IState PressSecondButton()
+    {
+        currentState = currentState.PressSecondButton();
+        return currentState;
+    }
+}
+
+internal class InitialScreenState : IState
+{
+    public string TextOnFirstButton => "Start";
+    public string TextOnSecondButton => "Lap";
+
+    public IState PressFirstButton()
+    {
+        return new RunningState();
+    }
+
+    public IState PressSecondButton()
+    {
+        
+        return this;
+    }
+}
+
+
+internal class RunningState : IState
+{
+    public string TextOnFirstButton => "Stop";
+    public string TextOnSecondButton => "Lap";
+
+    public IState PressFirstButton()
+    {
+        return new DisplayingResultState();
+    }
+
+    public IState PressSecondButton()
+    {
+        return new LappingState();
+    }
+}
+
+internal class LappingState : IState
+{
+    public string TextOnFirstButton => "Stop";
+    public string TextOnSecondButton => "Lap";
+
+    public IState PressFirstButton()
+    {
+        return new DisplayingResultState();
+    }
+
+    public IState PressSecondButton()
+    {
+        return new LappingState();
+    }
+}
+
+internal class DisplayingResultState : IState
+{
+    public string TextOnFirstButton => "Start";
+    public string TextOnSecondButton => "Reset";
+
+    public IState PressFirstButton()
+    {
+        return new RunningState();
+    }
+
+    public IState PressSecondButton()
+    {
+        return new InitialScreenState();
+    }
+}
